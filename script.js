1afd447d3ab233425dccdaf5327609b72ed878e8
Yashaswee-k <kandula280572@gmail.com>
2026-05-21 17:34:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function bookNow(){
+  alert("Booking page will open soon!");
+}
+
+function learnMore(){
+  alert("Explore more destinations and travel packages.");
+}
+
+function visitPlace(place){
+  alert("Welcome to " + place + "!");
+}
+
+function submitForm(event){
+  event.preventDefault();
+  alert("Message Sent Successfully!");
+}
